566953524f3413ecb177c185ea6513e5974476b3
Lance Rall <lancerall@charter.net>
2014-05-11 20:10:38 -0600

    Adding ability to specify token code in URL, multi-user support

--- stoken.php
+++ stoken.php
@@ -1,20 +1,36 @@
 <?php
-$current_code = exec("stoken --rcfile=/var/www/.stokenrc");
-#echo "<br>";
-#echo "\n";
-$file = '/var/www/lastRSA.txt';
-$file_code = file_get_contents($file);
-#echo "current code: $current_code <br>\n file code: $file_code";
-while($current_code == $file_code){
- #  echo "<br>sleeping....";
-   sleep(2);
-   $current_code = exec("stoken --rcfile=/var/www/.stokenrc");
- #  echo "<br>";
- #  echo "\n";   
-   $file_code = file_get_contents($file);
- #  echo "current code: $current_code <br>\n file code: $file_code <br>\n";
+// Usage: stoken.php?username=lrall&token=abc12300000...
+
+$token = "";
+$username = "";
+
+if (isset($_GET["token"]) && !isset($_GET["username"])) die("Token was provided but username not present.");
+if (!isset($_GET["token"]) && isset($_GET["username"])) die("Username was provided but token not present.");
+if (isset($token) && isset($username)) {
+	$token = $_GET["token"];
+	$username = $_GET["username"];
 }
-#echo "<br>\nreturning a code: ";
+
+if ($token) $command = "stoken --token=".$token;
+else $command = "stoken --rcfile=/var/www/.stokenrc";
+
+$current_code = exec($command);
+
+if ($username) $file = '/var/www/'.$username.'.txt';
+else $file = '/var/www/lastRSA.txt';
+
+if (file_exists($file)) $file_code = file_get_contents($file);
+else $file_code="";
+
+if (isset($_GET["debug"])) echo "current code: $current_code <br>\n file code: $file_code \n<br />";
+while($current_code == $file_code){
+	if (isset($_GET["debug"])) echo "<br>sleeping....\n<br />";
+	sleep(2);
+	$current_code = exec($command);
+	$file_code = file_get_contents($file);
+	if (isset($_GET["debug"])) echo "current code: $current_code <br>\n file code: $file_code <br>\n";
+}
+if (isset($_GET["debug"])) echo "<br>\nreturning a code: ";
 file_put_contents($file, $current_code);
 echo $current_code;
 ?>
